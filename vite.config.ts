9650195a4d1a463de2fe4199bbe7fa97f51cf001
Alan-459 <52850770+Alan-459@users.noreply.github.com>
2024-05-24 20:31:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "http://0.0.0.0",
-    port: 3000,
-  },
+  server:{
+    https: "http",
+    host: "0.0.0.0",
+    port: 3000}
+  ,
   plugins: [
     react(),
     VitePWA({
