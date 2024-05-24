54b34b7fde2d037cdc3dbbd9a3d571d07d03f8ad
Alan-459 <52850770+Alan-459@users.noreply.github.com>
2024-05-24 20:51:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
-    https: "http",
+    https: "https",
     host: "0.0.0.0",
     port: 3000}
   ,
