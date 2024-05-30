75d422e436984b2608514739263ef0ab0b54c122
RAKSHA V G <vgraksha26@gmail.com>
2024-05-30 16:57:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,8 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
-    https: "http",
     host: "0.0.0.0",
-    port: 3000}
+    port: 8087}
   ,
   plugins: [
     react(),
